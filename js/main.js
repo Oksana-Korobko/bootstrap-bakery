ad953ef8d2dc304bb85fc00be902ba96e9906983
Oksana Korobko <oksana.korobko.fd@gmail.com>
2024-02-15 14:58:19 +0200

    starting js

---
+++ js/main.js
@@ -0,0 +1,12 @@
+window.addEventListener('DOMContentLoaded', () => {
+
+    const novelty = document.querySelector('.novelty'),
+        map = document.querySelector('map'),
+        contacts = document.querySelector('contacts'),
+        assortment = document.querySelector('assortment'),
+        advantages = document.querySelector('advantages');
+    
+    function hideContent () {
+        
+    }
+})

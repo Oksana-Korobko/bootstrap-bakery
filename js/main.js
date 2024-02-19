8f66ab09b2703f13e0e5c9f72e93521ccb4a2ff3
Oksana Korobko <oksana.korobko.fd@gmail.com>
2024-02-19 11:11:07 +0200

    show/hide pages

--- js/main.js
+++ js/main.js
@@ -1,12 +1,24 @@
 window.addEventListener('DOMContentLoaded', () => {
 
-    const novelty = document.querySelector('.novelty'),
-        map = document.querySelector('map'),
-        contacts = document.querySelector('contacts'),
-        assortment = document.querySelector('assortment'),
-        advantages = document.querySelector('advantages');
+    const pages = document.querySelectorAll('.page'),
+          navPages = document.querySelectorAll('.menuItem ')
+        // novelty = document.querySelector('.novelty'),
+        // map = document.querySelector('map'),
+        // contacts = document.querySelector('contacts'),
+        // assortment = document.querySelector('assortment'),
+        // advantages = document.querySelector('advantages');
     
     function hideContent () {
-        
+        pages.forEach(item, ()=> {
+            item.style.display = 'none';
+        })
+        navPages.forEach(item, () => {
+            item.classlist.remove('active')
+        })
+    }
+
+    function showPage (i) {
+        pages[i].style.display = "block";
+        navPages[i].classlist.add('active')
     }
 })
